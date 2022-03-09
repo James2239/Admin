7fe46e71ce0bf8409e4995bb081bb70f7e3cfa2a
james239 <jamesngkaranja@gmail.com>
2022-03-09 10:07:01 +0300

    Functions

---
+++ defaultArguments.py
@@ -0,0 +1,7 @@
+
+# default arguments
+
+def student(firstname,lastname):
+    print(firstname,lastname)
+student(firstname='geeks',lastname='Practice')
+student(lastname='practice',firstname='geeks')
---
+++ modules.py
@@ -0,0 +1,7 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+from math import sqrt,factorial
+print(sqrt(16))
+print(factorial(16))
+print(factorial(16))
